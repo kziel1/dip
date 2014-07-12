6f9aced6b0fedaca064831b105d68db9906c8ae7
kziel1 <krzysztofzielinski862@gmail.com>
2014-07-12 14:13:44 +0200

    new algorithm

--- homework 5/Dip5.cpp
+++ homework 5/Dip5.cpp
@@ -9,43 +9,89 @@
 #include "Dip5.h"
 
 // uses structure tensor to define interest points (foerstner)
-void Dip5::getInterestPoints(Mat& img, double sigma, vector<KeyPoint>& points) {
-	int kernelSize = (int) (ceil(3 * sigma) + 1) - (kernelSize % 2);
+void Dip5::getInterestPoints(Mat& img, double sigma, vector<KeyPoint>& points){
+	int kernelSize = (int) ceil(3*sigma) + 1-kernelSize%2;
 	Mat fstdevKernelX =  createFstDevKernel(0);
-	// cout << "fstKernel = "<< endl << " "  << fstdevKernelX << endl << endl;
+	//cout << "fstKernel = "<< endl << " "  << fstdevKernelX << endl << endl;
 	Mat fstdevKernelY =  fstdevKernelX.t();
+	//cout << "fstKernel = "<< endl << " "  << fstdevKernelY << endl << endl;
 	Mat gradientsX;
 	filter2D(img, gradientsX, CV_32FC1, fstdevKernelX) ;
 	// showImage(gradientsX, "asd", 1, true, false);
 	Mat gradientsY;
 	filter2D(img, gradientsY, CV_32FC1, fstdevKernelY) ;
 	// showImage(gradientsY, "qwe", 0, true, false);
-	Mat structureTensor = Mat::zeros(2,2,CV_32FC1);
-	
-	int i, j;
-	Mat plesseyHarrisDetector = Mat::zeros(img.rows,img.cols,CV_32FC1);
-	
-	for (int x = kernelSize; x < img.rows - kernelSize; x++) for(int y=kernelSize;y<img.cols-kernelSize;y++) {
-		structureTensor = Mat::zeros(2,2,CV_32FC1);
-		for (int xw = 0; xw < kernelSize / 2; xw++) for(int yw = 0; yw < kernelSize; yw++) {
-			i = x + xw - kernelSize / 2;
-			j = y + yw - kernelSize / 2;
-			structureTensor.at<float>(0, 0) += gradientsX.at<float>(i,j) * gradientsX.at<float>(i,j);
-			structureTensor.at<float>(1, 1) += gradientsY.at<float>(i,j) * gradientsY.at<float>(i,j);
-			structureTensor.at<float>(1, 0) += gradientsX.at<float>(i,j) * gradientsY.at<float>(i,j);
-		}
-		structureTensor.at<float>(0, 1) = structureTensor.at<float>(1, 0);
-		float structureTensorTrace = sum(trace(structureTensor))[0];
-		plesseyHarrisDetector.at<float>(x, y) = determinant(structureTensor) - 0.04 * structureTensorTrace * structureTensorTrace;
- 	}
 
- 	plesseyHarrisDetector = nonMaxSuppression(plesseyHarrisDetector);
 
- 	for(int x = kernelSize; x < img.rows - kernelSize; x++) for(int y = kernelSize; y < img.cols - kernelSize; y++) {
-		if (abs(plesseyHarrisDetector.at<float>(x, y)) > 100000) {
-			points.push_back(KeyPoint(y,x,5));
+// 	Mat structureTensor = Mat::zeros(2,2,CV_32FC1);
+// 	int i,j;
+// 	Mat plesseyHarrisDetector = Mat::zeros(img.rows,img.cols,CV_32FC1);
+// 	for(int x=kernelSize;x<img.rows-kernelSize;x++){
+//  		for(int y=kernelSize;y<img.cols-kernelSize;y++){
+// structureTensor = Mat::zeros(2,2,CV_32FC1);
+//  			for(int xw=0;xw<kernelSize/2;xw++){
+//  				for(int yw=0;yw<kernelSize;yw++){
+//  					i=x+xw-kernelSize/2;
+//  					j=y+yw-kernelSize/2;
+// 					structureTensor.at<float>(0, 0)+=gradientsX.at<float>(i,j)*gradientsX.at<float>(i,j);
+//  					structureTensor.at<float>(1, 1)+=gradientsY.at<float>(i,j)*gradientsY.at<float>(i,j);
+//  					structureTensor.at<float>(1, 0)+=gradientsX.at<float>(i,j)*gradientsY.at<float>(i,j);
+//  				}
+//  			}
+//  			structureTensor.at<float>(0, 1)=structureTensor.at<float>(1, 0);
+// 			float structureTensorTrace = sum(trace(structureTensor))[0];
+//  			plesseyHarrisDetector.at<float>(x, y)=determinant(structureTensor)-0.04*structureTensorTrace*structureTensorTrace;
+//  		}
+//  	}
+//  	plesseyHarrisDetector = nonMaxSuppression(plesseyHarrisDetector);
+//  	for(int x=kernelSize;x<img.rows-kernelSize;x++){
+//  		for(int y=kernelSize;y<img.cols-kernelSize;y++){
+// 				if(abs(plesseyHarrisDetector.at<float>(x, y))>100000){
+//  				points.push_back(KeyPoint(y,x,5));
+//  			}
+//  		}
+//  	}
+
+int i,j;
+Mat gxgx = Mat::zeros(img.rows,img.cols,CV_32FC1);
+Mat gygy = Mat::zeros(img.rows,img.cols,CV_32FC1);
+Mat gxgy = Mat::zeros(img.rows,img.cols,CV_32FC1);
+for(int x=0;x<img.rows;x++){
+  		for(int y=0;y<img.cols;y++){
+			gxgx.at<float>(x, y)=gradientsX.at<float>(x, y)*gradientsX.at<float>(x, y);
+			gygy.at<float>(x, y)=gradientsY.at<float>(x, y)*gradientsY.at<float>(x, y);
+			gxgy.at<float>(x, y)=gradientsX.at<float>(x, y)*gradientsY.at<float>(x, y);
+  		}
+  	}
+Mat avggxgx=Mat::zeros(img.rows,img.cols,CV_32FC1);
+Mat avggygy=Mat::zeros(img.rows,img.cols,CV_32FC1);
+Mat avggxgy=Mat::zeros(img.rows,img.cols,CV_32FC1);
+
+GaussianBlur( gxgx, avggxgx, Size( kernelSize, kernelSize ), sigma);
+GaussianBlur( gygy, avggygy, Size( kernelSize, kernelSize ), sigma);
+GaussianBlur( gxgy, avggxgy, Size( kernelSize, kernelSize ), sigma);
+
+Mat traces=Mat::zeros(img.rows,img.cols,CV_32FC1);
+for(int x=0;x<img.rows;x++){
+  		for(int y=0;y<img.cols;y++){
+			traces.at<float>(x, y)=avggxgx.at<float>(x, y)+avggygy.at<float>(x, y);
 		}
- 	}
+  	}
+
+Mat determinants=Mat::zeros(img.rows,img.cols,CV_32FC1);
+for(int x=0;x<img.rows;x++){
+  		for(int y=0;y<img.cols;y++){
+			determinants.at<float>(x, y)=avggxgx.at<float>(x, y)*avggygy.at<float>(x, y)-avggxgy.at<float>(x, y)*avggxgy.at<float>(x, y);
+		}
+  	}
+
+// Mat weights=Mat::zeros(img.rows,img.cols,CV_32FC1);
+// for(int x=0;x<img.rows;x++){
+//   		for(int y=0;y<img.cols;y++){
+// 			weights.at<float>(x, y)=avggxgx.at<float>(x, y);//*avggygy.at<float>(x, y)-avggxgy.at<float>(x, y)*avggxgy.at<float>(x, y);
+// 		}
+//   	}
+// showImage(determinants, "qwe", 0, true, false);
 }
 
 // creates kernel representing fst derivative of a Gaussian kernel in x-direction
@@ -54,18 +100,19 @@ sigma	standard deviation of the Gaussian kernel
 return	the calculated kernel
 */
 Mat Dip5::createFstDevKernel(double sigma){
-	
-	sigma = this->sigma;
-	int kernelSize = (int) ceil(3 * sigma) + 1 - kernelSize % 2;
+	sigma=this->sigma;
+	int kernelSize = (int) ceil(3*sigma) + 1-kernelSize%2;
 	Mat gaussianKernelX = getGaussianKernel(kernelSize,sigma, CV_32FC1);
 	Mat gaussianKernelY = getGaussianKernel(kernelSize,sigma, CV_32FC1);
 	Mat gaussianKernel = gaussianKernelX*gaussianKernelY.t();
 	Mat fstKernel = Mat::ones(kernelSize, kernelSize, CV_32FC1);
- 	for (int x = 0; x < kernelSize; x++) for(int y = 0; y < kernelSize; y++) {
-		int rx=x-kernelSize / 2;
-		fstKernel.at<float>(x, y) =- rx * gaussianKernel.at<float>(x,y) / (sigma * sigma);
+ 	for(int x=0;x<kernelSize;x++){
+ 		for(int y=0;y<kernelSize;y++){
+ 			int rx=x-kernelSize/2;
+ 			fstKernel.at<float>(x, y)=-rx*gaussianKernel.at<float>(x,y)/sigma/sigma;
+ 		}
  	}
-
+	//cout << "fstKernel = "<< endl << " "  << fstKernel << endl << endl;
 	return fstKernel;
 }
 
